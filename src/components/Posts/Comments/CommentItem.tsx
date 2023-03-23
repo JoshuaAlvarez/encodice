6d8fb47087d5e6bbb8263d16a61f9fee321a3acc
JoshuaAlvarez <kornnlore@gmail.com>
2023-03-22 21:24:47 -0600

    Fixed search function

--- src/components/Posts/Comments/CommentItem.tsx
+++ src/components/Posts/Comments/CommentItem.tsx
@@ -56,15 +56,15 @@ const CommentItem: React.FC<CommentItemProps> = ({
           <Icon as={IoArrowDownCircleOutline} />
           {userId === comment.creatorId && (
             <>
-              <Text fontSize={"9pt"} _hover={{ color: "blue.500" }}>
-                Edit
-              </Text>
+              {/* <Text fontSize={"9pt"} _hover={{ color: "blue.500" }}>
+                Editar
+              </Text> */}
               <Text
                 fontSize={"9pt"}
                 _hover={{ color: "blue.500" }}
                 onClick={() => onDeleteComment(comment)}
               >
-                Delete
+                Borrar
               </Text>
             </>
           )}
